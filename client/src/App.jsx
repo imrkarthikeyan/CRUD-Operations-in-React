6bbb831608c9e2cb2eaafbf2ac684f75ff7ebfd2
Karthikeyan R <2303737720521034@ksrct.net>
2025-05-02 13:38:23 +0530

    implemented update user and add user methods

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 
   const [users,setUsers]=useState([])
   const [filterUsers,setFilterUsers]=useState([])
+  const [isModelOpen, setIsModelOpen]=useState(false)
+  const [userData, setUserData]=useState({name:"",age:"",city:""})
 
   const getAllUsers=async()=>{
     await axios.get("http://localhost:8000/users").then((res)=>{
@@ -27,19 +29,67 @@ function App() {
     setFilterUsers(filteredUsers)
   }
 
+  //Delete Function
+  const handleDelete=async(id)=>{
+    const isConfirmed=window.confirm("Are you sure you want to delete this user?")
+    if(isConfirmed){
+      await axios.delete(`http://localhost:8000/users/${id}`).then((res)=>{
+        setUsers(res.data)
+        setFilterUsers(res.data)
+      })
+    }
+  }
 
+  //Add user Detail
+  const handleAddRecord=()=>{
+    setUserData({name:"",age:"",city:""})
+    setIsModelOpen(true)
+  }
+
+  const handleData=(e)=>{
+    setUserData({...userData,[e.target.name]:e.target.value})
+  }
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    if(userData.id){
+      await axios.patch(`http://localhost:8000/users/${userData.id}`,userData).then((res)=>{
+        console.log(res)
+      })
+    }
+    else{
+      await axios.post("http://localhost:8000/users",userData).then((res)=>{
+        console.log(res)
+      })
+    }
+    getAllUsers()
+    setIsModelOpen(false)
+    setUserData({name:"",age:"",city:""})
+  }
+
+  //Update user
+  const handleUpdateRecord=(user)=>{
+    setUserData(user)
+    setIsModelOpen(true);
+  }
+
+  //Close Modal
+  const closeModal=()=>{
+    getAllUsers()
+    setIsModelOpen(false)
+  }
 
   return (
     <>
       <div className='container'>
-        <h2>CRUD Application</h2>
+        <h1>CRUD Application</h1>
         <div className="input-search">
           <input
             type='search'
             placeholder='Search text'
             onChange={handleSearchChange}
           />
-          <button className='btn'>Add Record</button>
+          <button onClick={handleAddRecord} className='btn'>Add Record</button>
         </div>
         <table className='table-container'>
           <thead>
@@ -60,13 +110,34 @@ function App() {
                 <td>{user.name}</td>
                 <td>{user.age}</td>
                 <td>{user.city}</td>
-                <td><button className='btn green'>Edit</button></td>
-                <td><button className='btn red'>Delete</button></td>
+                <td><button className='btn green' onClick={()=>handleUpdateRecord(user)}>Edit</button></td>
+                <td><button onClick={()=>handleDelete(user.id)} className='btn red'>Delete</button></td>
               </tr>
               )
             })}
           </tbody>
         </table>
+        {isModelOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span onClick={closeModal} className="close">&times;</span>
+              <h2>User Record</h2>
+              <div className="input-container">
+                <label htmlFor='name'>Name : </label>
+                <input type='text' name='name' id='name' value={userData.name} onChange={handleData}></input>
+              </div>
+              <div className="input-container">
+                <label htmlFor='age'>Age : </label>
+                <input type='number' name='age' id='age' value={userData.age} onChange={handleData}></input>
+              </div>
+              <div className="input-container">
+                <label htmlFor='city'>City : </label>
+                <input type='text' name='city' id='city' value={userData.city} onChange={handleData}></input>
+              </div>
+              <button className='btn green' onClick={handleSubmit}>Add User</button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   )
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const fs=require("fs")
 
 const app=express()
+app.use(express.json())
 const port=8000
 app.use(
     cors({
@@ -16,7 +17,45 @@ app.get("/users", (req,res)=>{
     return res.json(users)
 })
 
+//Delete user
+app.delete("/users/:id",(req,res)=>{
+    let id=Number(req.params.id)
+    let filteredUsers=users.filter((user)=>user.id!==id)
+    fs.writeFile("./sample.json",JSON.stringify(filteredUsers),(err,data)=>{
+        return res.json(filteredUsers)
+    })
+})
 
+//Add user
+app.post("/users",(req,res)=>{
+    let {name,age,city}=req.body
+    if(!name || !age || !city){
+        res.status(400).send({message:"All Fields Required"})
+    }
+    let id=Date.now();
+    users.push({id,name,age,city})
+
+    fs.writeFile("./sample.json",JSON.stringify(users),(err,data)=>{
+        return res.json({message:"User detail added successfully"})
+    })
+})
+
+//Update user
+app.patch("/users/:id",(req,res)=>{
+    let id=Number(req.params.id)
+    let {name,age,city}=req.body
+    if(!name || !age || !city){
+        res.status(400).send({message:"All Fields Required"})
+    }
+    let index=users.findIndex((user)=> user.id==id)
+
+    users.splice(index, 1, { id, name, age, city });
+
+
+    fs.writeFile("./sample.json",JSON.stringify(users),(err,data)=>{
+        return res.json({message:"User detail updated successfully"})
+    })
+})
 
 app.listen(port, (err)=>{
     console.log(`App is running in port ${port}`)
